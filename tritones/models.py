eb2ad6330c9271eb797f13f752ddf3cfeebcb672
ShawnMalal <109571508+ShawnMalal@users.noreply.github.com>
2023-09-17 06:35:48 -0700

    Added image field for each member

--- tritones/models.py
+++ tritones/models.py
@@ -5,17 +5,9 @@ from django.db import models
 class Member(models.Model):
     name = models.CharField(max_length=30)
     number = models.CharField(max_length=10) # has special chars so have to use charField
-    voicePart = models.CharField(max_length=30)
+    voicePart = models.CharField(max_length=30, null=True, blank=True)
     board = models.CharField(max_length=50, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
-
-class boardMember(models.Model):
-    name = models.CharField(max_length=30)
-    number = models.CharField(max_length=10) # has special chars so have to use charField
-    voicePart = models.CharField(max_length=30)
-    board = models.CharField(max_length=50)
+    imageUrl = models.CharField(max_length=1000, null=True, blank=True)
 
     def __str__(self):
         return self.name
